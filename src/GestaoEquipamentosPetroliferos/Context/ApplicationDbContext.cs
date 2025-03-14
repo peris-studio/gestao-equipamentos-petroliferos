a222fee21693809e15d7820aacc31472b2638253
Hisllaylla Kézia <hisllayllak@gmail.com>
2025-03-13 23:01:26 -0300

    [feat]: add Context

---
+++ src/GestaoEquipamentosPetroliferos/Context/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+namespace GestaoEquipamentosPetroliferos.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Alerta> Alertas { get; set; } = null!;
+    public DbSet<Equipamento> Equipamentos { get; set; } = null!;
+    public DbSet<HistoricoFalha> HistoricoFalhas { get; set; } = null!;
+    public DbSet<IncidenteSeguranca> IncidentesSeguranca { get; set; } = null!;
+    public DbSet<Manutencao> Manutencoes { get; set; } = null!;
+    public DbSet<Pecas> Pecas { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // mapeamentos
+        modelBuilder.ApplyConfiguration(new AlertaMap());
+        modelBuilder.ApplyConfiguration(new EquipamentoMap());
+        modelBuilder.ApplyConfiguration(new HistoricoFalhaMap());
+        modelBuilder.ApplyConfiguration(new IncidenteSegurancaMap());
+        modelBuilder.ApplyConfiguration(new ManutencaoMap());
+        modelBuilder.ApplyConfiguration(new PecaMap());
+
+        // Filtro automático para só trazer registros ativos
+        modelBuilder.Entity<Alerta>().HasQueryFilter(x => x.Ativo);
+        modelBuilder.Entity<Equipamento>().HasQueryFilter(x => x.Ativo);
+        modelBuilder.Entity<HistoricoFalha>().HasQueryFilter(x => x.Ativo);
+        modelBuilder.Entity<IncidenteSeguranca>().HasQueryFilter(x => x.Ativo);
+        modelBuilder.Entity<Manutencao>().HasQueryFilter(x => x.Ativo);
+        modelBuilder.Entity<Peca>().HasQueryFilter(x => x.Ativo);
+
+        // para buscar inativos, use .IgnoreQueryFilters()
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+
+    public new async Task<bool> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await base.SaveChangesAsync(cancellationToken) > 0;
+    }
+}
